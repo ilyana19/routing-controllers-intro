a6686ddbb245bd2bfc26319786dd2cb831faefc8
ilyana19 <aries_2152@hotmail.com>
2018-08-08 15:06:16 -0400

    added a secrets page. Type 'baccano' to access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,4 +22,13 @@ class PagesController < ApplicationController
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
+
+  def secrets
+    if params[:magic_word] == 'baccano'
+      render :secrets
+    else
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to "/welcome"
+    end
+  end
 end
