1de184ff01f0d9640fde2f7db2ebc1411dcc4e05
ilyana19 <aries_2152@hotmail.com>
2018-08-08 15:17:02 -0400

    added route helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     @header = "This is the contest page"
     # render :contest
     flash[:notice] = "Sorry, the contest has ended"
-    redirect_to "/welcome"
+    redirect_to welcome_path
   end
 
   def kitten
@@ -28,7 +28,7 @@ class PagesController < ApplicationController
       render :secrets
     else
       flash[:alert] = "Sorry, you're not authorized to see that page!"
-      redirect_to "/welcome"
+      redirect_to welcome_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/' => 'pages#welcome'
+  # get '/' => 'pages#welcome'
+  root 'pages#welcome'
+
+
   get '/welcome' => 'pages#welcome'
   get '/contest' => 'pages#contest'
 
-  get '/kitten/:size' => 'pages#kitten'
-  get '/kittens/:size' => 'pages#kittens'
+  # get '/kitten/:size' => 'pages#kitten'
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
 
-  get '/secrets/:magic_word' => 'pages#secrets'
+  # get '/kittens/:size' => 'pages#kittens'
+  get '/kittens:size' => 'pages#kittens', as: 'cats'
+
+  # get '/secrets/:magic_word' => 'pages#secrets'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 end
